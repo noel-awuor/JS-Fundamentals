2c782f07937d5e33528da57e88d02015d46c0135
Noel Awuor <nouellawuor@gmail.com>
2025-07-14 00:03:17 +0300

    number

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,10 @@
 const first = process.argv[2];
 const isNumber = isNaN(Number(first))
 
+console.log(first, isNumber)
+
 if (isNumber) {
-    console.log(`My number: ${first}`)
+    console.log(`My number: ${Number.parseInt(first)}`)
 } else {
     console.log("Not a number")
 }
